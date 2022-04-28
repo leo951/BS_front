7f97b7c5caaa15341df73620519589e73dd621f7
Léo Peyré <leo.peyre95@gmail.com>
2022-04-28 22:46:14 +0200

    Animation logo + details css

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -3,14 +3,15 @@ import React from "react";
 import styles from "./Logo.module.scss";
 import Link from "next/link";
 
-
 const Logo = (props) => {
   return (
     <>
       {props.forNav == true ? (
-        <Link href="/">
-          <div className={styles.logoForNav}>Parkmanager Corp</div>
-        </Link>
+        <div className={styles.logo__container}>
+          <Link href="/">
+            <button className={styles.logoForNav}>Parkmanager Corp</button>
+          </Link>
+        </div>
       ) : (
         <div className={styles.logo}>Parkmanager Corp</div>
       )}
